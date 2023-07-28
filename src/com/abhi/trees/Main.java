d9698b9a76ee18edd0f875305bbf2af54c37758b
Abhinav Kumar <abhinav.kumar@intelliswift.com>
2023-07-28 23:49:50 +0530

    dummy changes for tesing merge conflict

--- src/com/abhi/trees/Main.java
+++ src/com/abhi/trees/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Node root= Node.createTree();
         //FindMaxElementInTree
+        //demoooo
       int result=  FindMaxElementInTree.findMaxElement(root);
 
         System.out.println("maximum value in tree is : "+result );
